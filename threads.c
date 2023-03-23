8a2cca1a46d0970c6db74cd34e3cc25e4ca925ce
akshaykhoje <khojeakshay02@gmail.com>
2023-03-23 11:51:41 +0530

    threads.c complete

--- threads.c
+++ threads.c
@@ -4,20 +4,20 @@
 
 void *fun1(void *arg)       //this becomes the function pointer (start point of thread)
 {
-    while (1)
+    for(int i=0; i<8; i++)
     {
         sleep(1);
-        printf("fun1\n");
+        printf("fun1 : %d\n", i);
     }
     return NULL;
 }
 
 void function2()
 {
-    while (1)
+    for(int i=0; i<3; i++)
     {
         sleep(2);
-        printf("function2\n");
+        printf("function2 : %d\n", i);
     }
     return;
 }
@@ -28,5 +28,11 @@ int main(void)
     pthread_create(&newthread, NULL, fun1, NULL);      // create a new thread and give a start routine
     // fun1();
     function2();            // this now becomes the second thread
+    pthread_join(newthread, NULL);
+
     return 0;
 }
+
+/*
+At this point, the program terminates once one of the thread completes its execution. To avoid this, we join the threads using pthread_join()
+*/
