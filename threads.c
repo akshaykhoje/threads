dd145b903808bc20e4535d198d90241ff19f2762
akshaykhoje <khojeakshay02@gmail.com>
2023-03-25 09:55:51 +0530

    passing arguments to thread from main

--- threads.c
+++ threads.c
@@ -4,10 +4,12 @@
 
 void *fun1(void *arg)       //this becomes the function pointer (start point of thread)
 {
+    int *iptr = (int *)arg;
     for(int i=0; i<8; i++)
     {
         sleep(1);
-        printf("fun1 : %d\n", i);
+        printf("fun1 : %d\t v : %d\n", i, *iptr);
+        (*iptr)++;
     }
     return NULL;
 }
@@ -25,10 +27,13 @@ void function2()
 int main(void)
 {
     pthread_t newthread;        // create a new thread variable
-    pthread_create(&newthread, NULL, fun1, NULL);      // create a new thread and give a start routine
+    int v = 5;                  // argument
+
+    pthread_create(&newthread, NULL, fun1, &v);      // create a new thread and give a start routine
     // fun1();
     function2();            // this now becomes the second thread
     pthread_join(newthread, NULL);
+    printf("thread's done! v : %d\n", v);
 
     return 0;
 }
